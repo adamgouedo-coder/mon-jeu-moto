db6b71e7f457e7e10dcb486b8f0f1ed3336e97a7
adamgouedo-coder <adam.gouedo@ndtramontane.education>
2026-05-12 17:11:09 +0200

    Update scores.js

--- api/scores.js
+++ api/scores.js
@@ -5,20 +5,20 @@ const filePath = path.join(process.cwd(), "scores.json");
 
 export default function handler(req, res) {
 
+  // GET = lire scores
   if (req.method === "GET") {
 
-    const data =
-      JSON.parse(fs.readFileSync(filePath));
+    const data = JSON.parse(fs.readFileSync(filePath));
 
     data.sort((a, b) => b.score - a.score);
 
     return res.status(200).json(data.slice(0, 10));
   }
 
+  // POST = ajouter score
   if (req.method === "POST") {
 
-    const data =
-      JSON.parse(fs.readFileSync(filePath));
+    const data = JSON.parse(fs.readFileSync(filePath));
 
     const newScore = {
       username: req.body.username,
@@ -27,14 +27,9 @@ export default function handler(req, res) {
 
     data.push(newScore);
 
-    fs.writeFileSync(
-      filePath,
-      JSON.stringify(data, null, 2)
-    );
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 
-    return res.status(200).json({
-      success: true
-    });
+    return res.status(200).json({ success: true });
   }
 
   res.status(405).end();
